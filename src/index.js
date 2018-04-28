e7d821506e4b11cdc48223c588125851132c57ba
SrKomodo <komodosr@gmail.com>
2018-04-28 18:33:18 -0300

    add konami code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./index.scss";
+
+const code = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+let konami = 0;
+
+document.addEventListener("keypress", e => {
+  if (e.key === code[konami]) konami++;
+  else konami = 0;
+  if (konami === code.length) {
+    konami = 0;
+    document.body.className = "konami";
+  }
+});
