316d32f65303a85877cb8faf188e327474e48500
SrKomodo <komodosr@gmail.com>
2018-07-19 13:18:02 -0300

    change text between commie and capitalist versions

--- src/index.js
+++ src/index.js
@@ -17,6 +17,11 @@ Actually I wanted to put some cool easter egg here but it's 2AM and I'm too tire
 ╚════╩════╝
 `);
 
+const text = [
+  `<h3>Why Is this layout so weird?</h3><p><em>In Soviet Russia CSS writes you.</em></p><p>If you dislike fully automated luxury gay space communism you can input the konami code for a more capitalist version.</p>`,
+  `<h3>Why Is this layout so weird?</h3><p><em>Better dead than red amirite?</em></p><p>If you are an evil commie you can input the konami code to go back to Moscow</p>`
+];
+
 const code = [
   "ArrowUp",
   "ArrowUp",
@@ -31,11 +36,19 @@ const code = [
 ];
 let konami = 0;
 
+const article = document.querySelector("article:nth-child(3)");
+
 document.addEventListener("keydown", e => {
   if (e.key === code[konami]) konami++;
   else konami = 0;
   if (konami === code.length) {
     konami = 0;
-    document.body.classList.toggle("konami");
+    if (document.body.className === "konami") {
+      document.body.className = "";
+      article.innerHTML = text[0];
+    } else {
+      document.body.className = "konami";
+      article.innerHTML = text[1];
+    }
   }
 });
